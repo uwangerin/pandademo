de9309d1413c963ade963de9816768bae91d124e
wolfbo <wolfgang@boremski.net>
2017-07-09 21:08:45 +0200

    use twig
    commit is made from other pc, as we can not install a private php-serer on the initial notebook

--- web/index.php
+++ web/index.php
@@ -9,5 +9,78 @@ $twig = new Twig_Environment($loader, array(
 if ($_SERVER['REQUEST_URI'] == "/") $template = 'index'; 
 else $template = substr($_SERVER['REQUEST_URI'], 1);
 $templatefile = $template.'.html.twig'; 
-echo $twig->render($templatefile, array('filename' => $template));
+
+
+if ($_SERVER['REQUEST_URI'] == '/') {
+
+
+	$teasers = array();
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	$sideteasers = array();
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	echo $twig->render('homepage.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
+}
+
+
+
+if ($_SERVER['REQUEST_URI'] == '/spielende-pandas') {
+	$teasers = array();
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	$sideteasers = array();
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	echo $twig->render('play.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
+}
+
+
+
+if ($_SERVER['REQUEST_URI'] == '/faule-pandas') {
+	$teasers = array();
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	$sideteasers = array();
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	echo $twig->render('foul.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
+}
+
+
+if ($_SERVER['REQUEST_URI'] == '/fressende-pandas') {
+	$teasers = array();
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$teasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	$sideteasers = array();
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßerer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+
+	echo $twig->render('eat.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
+}
+
+
 

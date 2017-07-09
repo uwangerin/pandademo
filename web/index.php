fe0f1d13ab5b865eef5d754d6aa837acbeeac314
Ulrike Wangerin <ulrike.wangerin@intosite.de>
2017-07-09 22:10:19 +0200

    shange images

--- web/index.php
+++ web/index.php
@@ -21,9 +21,9 @@ if ($_SERVER['REQUEST_URI'] == '/') {
 	$teasers[] = array('file'  => 'suess1.jpeg', 'title' => 'Sportlicher Panda');
 
 	$sideteasers = array();
-	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
-	$sideteasers[] = array('file'  => 'spiel6.jpeg', 'title' => 'Rückwärtsrolle');
-	$sideteasers[] = array('file'  => 'faul3.jpeg', 'title' => 'Zu zweit');
+	$sideteasers[] = array('file'  => 'pandaw.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'spiel6w.jpeg', 'title' => 'Rückwärtsrolle');
+	$sideteasers[] = array('file'  => 'faul3w.jpeg', 'title' => 'Zu zweit');
 
 	echo $twig->render('homepage.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
 }
@@ -38,9 +38,9 @@ if ($_SERVER['REQUEST_URI'] == '/spielende-pandas') {
 	$teasers[] = array('file'  => 'spiel6.jpeg', 'title' => 'Rückwärtsrolle');
 
 	$sideteasers = array();
-	$sideteasers[] = array('file'  => 'faul2.jpeg', 'title' => 'Abhängen');
-	$sideteasers[] = array('file'  => 'fress2.jpeg', 'title' => 'Noch mehr Bambus!');
-	$sideteasers[] = array('file'  => 'suess1.jpeg', 'title' => 'Sportlicher Panda');
+	$sideteasers[] = array('file'  => 'faul2w.jpeg', 'title' => 'Abhängen');
+	$sideteasers[] = array('file'  => 'fress2w.jpeg', 'title' => 'Noch mehr Bambus!');
+	$sideteasers[] = array('file'  => 'suess1w.jpeg', 'title' => 'Sportlicher Panda');
 
 	echo $twig->render('play.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
 }
@@ -55,9 +55,9 @@ if ($_SERVER['REQUEST_URI'] == '/faule-pandas') {
 	$teasers[] = array('file'  => 'faul4.jpeg', 'title' => 'Auf der Brücke');
 
 	$sideteasers = array();
-	$sideteasers[] = array('file'  => 'fress1.jpeg', 'title' => 'Lecker Bambus!');
-	$sideteasers[] = array('file'  => 'spiel5.jpeg', 'title' => 'Spaß im Korb');
-	$sideteasers[] = array('file'  => 'panda.jpg', 'title' => 'Süßer Panda');
+	$sideteasers[] = array('file'  => 'fress1w.jpeg', 'title' => 'Lecker Bambus!');
+	$sideteasers[] = array('file'  => 'spiel5w.jpeg', 'title' => 'Spaß im Korb');
+	$sideteasers[] = array('file'  => 'pandaw.jpg', 'title' => 'Süßer Panda');
 
 	echo $twig->render('foul.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
 }
@@ -71,9 +71,9 @@ if ($_SERVER['REQUEST_URI'] == '/fressende-pandas') {
 	$teasers[] = array('file'  => 'fress4.jpeg', 'title' => 'Mampf!');
 
 	$sideteasers = array();
-	$sideteasers[] = array('file'  => 'suess2.jpeg', 'title' => 'Fauler Panda');
-	$sideteasers[] = array('file'  => 'faul3.jpeg', 'title' => 'Zu zweit');
-	$sideteasers[] = array('file'  => 'spiel1.jpeg', 'title' => 'Raufbolde');
+	$sideteasers[] = array('file'  => 'suess2w.jpeg', 'title' => 'Fauler Panda');
+	$sideteasers[] = array('file'  => 'faul3w.jpeg', 'title' => 'Zu zweit');
+	$sideteasers[] = array('file'  => 'spiel1w.jpeg', 'title' => 'Raufbolde');
 
 	echo $twig->render('eat.html.twig', array('teasers' => $teasers, 'sideteasers' => $sideteasers));
 }
